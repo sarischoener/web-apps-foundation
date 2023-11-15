47d734856fd89dba352fd3bd21ed2bf2975f57b6
Sarah Schoener <sarahv.schoener@gmail.com>
2023-11-15 11:46:23 +0100

    chore:adds to-do-app

--- to-do-app/script.js
+++ to-do-app/script.js
@@ -1,32 +1,159 @@
 "use strict";
 
-const ulElement = document.querySelector("ul"); // ul auswählen + Variable kreieren
-const newToDoInputElement = document.querySelector("#new-todo"); // siehe 2.
-const btnNewToDoElement = document.querySelector("#btn-new-todo"); // siehe 2.
-// A) Displaying - Anzeigen der Todos:
-const toDos = ["Lerne HTML", "Lerne CSS", "Lerne JS"];
-// wir zählen durch ein Array die Todos auf
-
-// B) um in der Console durch jedes einzelne Todo zu gehen benötigen wir einen FOR OF Schleife
-for (let thetoDo of toDos) {
-  // C) Erstellen eines li node pro ToDo, um es ausgeben zu können
-  const li = document.createElement("li");
-
-  //===================== Zwei Möglichkeiten ==============
-  // li einen Text zuweisen --> thetoDo ist das einzelne toDo Beispiel Lerne HTML
-  // D) li.innerText = thetoDo;
-
-  // oder D) Nico: Wenn wir nur Text zuweisen wollen--> TextNode erstellen
-  const liText = document.createTextNode(thetoDo);
-  // E) liText muss ein Kind von li werden
-  li.appendChild(liText); // um li sichtbar zu machen li in das ul einhängen
-  ulElement.appendChild(li);
-  console.log(li);
+// Welche Elemente müssen interaktiv sein?
+// ---> Variablen dafür bauen!
+const newToDoInput = document.getElementById("newToDoInput");
+const addToDoBtn = document.getElementById("add-todo");
+const removeBtn = document.getElementById("remove");
+const radioInputAll = document.getElementById("radioInputAll");
+const radioInputDone = document.getElementById("radioInputDone");
+const radioInputOpen = document.getElementById("radioInputOpen");
+// am Anfang haben wir ein leeres Objekt.
+let state = {};
+// ### Local Storage
+// sichern
+function safeStateToLocalStorage() {
+  localStorage.setItem("state", JSON.stringify(state));
 }
-// Weitere Schritte:
-//  Feature: Add new ToDos
-// 1. Input & Button in HTML kreieren --> für Zugriff entweder Id oder class
-// 2. Für den Zugriff Input & Button je als Variable const + querySelector
-// 3. Eventlistener auf den Button
-// 4. Im Callblack Input auslesen
-// 5. Eingabe li kreieren
+// ### Local Storage
+// auslesen
+function readStateFromLocalStorage() {
+  let state = {};
+  const todosFromStorage = localStorage.getItem("state");
+  if (todosFromStorage !== null) {
+    state = JSON.parse(todosFromStorage);
+  }
+  return state;
+}
+state = readStateFromLocalStorage();
+
+// ### Displaying Todos:
+// ----> Mock-up:
+/*let state = {
+    "1686512042647": { 
+        description: "Lern JavaScript",
+        done: false
+    },
+    "1686512133550": {
+        description: "Lern CSS", 
+        done: false 
+    },
+    "1686512133551": {
+        description: "Lern HTML", 
+        done: false 
+    },
+};*/
+
+// State Management: ----> Visualisieren meiner Todos/ "Rendern"
+function renderTodos(filterForOpen = false, filterForDone = false) {
+  // meine Liste soll anfangs immer leer sein.
+  const list = document.getElementById("list");
+  list.innerHTML = "";
+  // forEach Todo in der Liste soll das gemacht werden:
+  Object.keys(state).forEach((id) => {
+    // wie greife ich auf die einzelnen todos zu?
+    const todo = state[id];
+    // ### Filtering Todos
+    if (filterForOpen === true && todo.done === true) {
+      return;
+    }
+    if (filterForDone === true && todo.done === false) {
+      return;
+    }
+    // todoListe bauen
+    const newLi = document.createElement("li");
+    // Checkbox bauen
+    const checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    checkbox.checked = todo.done;
+    // description der Todos bauen
+    const todoText = document.createElement("lable");
+    todoText.innerHTML = todo.description;
+    // todoList, Checkbox und Text hinzufügen
+    list.appendChild(newLi);
+    newLi.appendChild(checkbox);
+    newLi.append(todoText);
+    // line-through muss hier auch schon passieren!
+    if (todo.done === true) {
+      todoText.style.textDecoration = "line-through";
+    }
+    // ### Done status of Todos
+    // Checkbox klicken === true/done PLUS: der Todo Text soll durchgestrichen werden
+    // Checkbox wieder klicken === false/open
+    checkbox.addEventListener("change", (event) => {
+      console.log(event.target.checked);
+      if (event.target.checked) {
+        todoText.style.textDecoration = "line-through";
+        todo.done = true;
+      } else {
+        todoText.style.textDecoration = "none";
+        todo.done = false;
+      }
+      safeStateToLocalStorage();
+    });
+  });
+}
+// Funktion aufrufen um den State visuell darzustellen/ zu sehen
+renderTodos();
+
+// ### Adding new Todos
+// ---> Click-Event für "addToDo" bauen: Wenn ich auf den addToDoBtn klicke...
+addToDoBtn.addEventListener("click", () => {
+  //Variable für ein neues Todo PLUS ".trim", um Leerzeichen zu löschen:
+  const newDescription = newToDoInput.value.trim();
+  // Kein leeres oder unnötiges ToDo anlegen!
+  if (newDescription.length < 4) {
+    return;
+  }
+  // ### Duplicate Check
+  const arrayOfTodoIds = Object.keys(state);
+  const arrayOfAllDescriptions = arrayOfTodoIds.map(
+    (id) => state[id].description
+  );
+  if (arrayOfAllDescriptions.includes(newDescription)) {
+    return;
+  }
+  // ---> ansonsten erstelle das neue ToDo PLUS eigene ID:
+  const newId = new Date().getTime();
+  state[newId] = { description: newDescription, done: false };
+  //Inputfeld wieder leeren, nachdem ein To hinzugefügt wurde:
+  newToDoInput.value = "";
+  // in local storage safen:
+  safeStateToLocalStorage();
+  // state updaten:
+  renderTodos();
+});
+
+// ### Filtering Todos
+// Wenn ich auf Open klicke...
+radioInputOpen.addEventListener("click", () => {
+  renderTodos(true, false);
+});
+// Wenn ich auf Done klicke...
+radioInputDone.addEventListener("click", () => {
+  renderTodos(false, true);
+});
+// Wenn ich auf All klicke...
+radioInputAll.addEventListener("click", () => {
+  renderTodos(false, false);
+});
+
+// ### Remove done Todos
+removeBtn.addEventListener("click", () => {
+  let newState = {};
+
+  Object.keys(state).forEach((id) => {
+    const todo = state[id];
+
+    if (todo.done === false) {
+      newState[id] = todo;
+    }
+  });
+
+  state = newState;
+
+  // in local storage safen:
+  safeStateToLocalStorage();
+  // state updaten:
+  renderTodos();
+});
